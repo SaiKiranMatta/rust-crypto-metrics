577dd4d2de43a5eb99f41d2066e1dfa2a6011175
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-10-06 11:58:12 +0530

    chore: update earnings scraper api

--- src/routes/earning_scraper.rs
+++ src/routes/earning_scraper.rs
@@ -5,11 +5,14 @@ use actix_web::{
     HttpResponse,
 };
 use serde::Deserialize;
+use std::env;
+use dotenv::dotenv;
 
 #[derive(Debug, Deserialize)]
 pub struct FetchEarningsParams {
     pub interval: String,
     pub start_time: i64,
+    pub secret: String,
 }
 
 #[post("/earnings_scraper")]
@@ -17,6 +20,14 @@ pub async fn fetch_and_store_earnings(
     db: Data<Database>,
     params: Json<FetchEarningsParams>,
 ) -> HttpResponse {
+    dotenv().ok();
+
+    let expected_secret = env::var("SECRET_KEY").unwrap_or_else(|_| "default_secret".to_string());
+
+    if params.secret != expected_secret {
+        return HttpResponse::Unauthorized().body("Wrong secret key.");
+    }
+
     match services::earnings_fetcher::fetch_and_store_earnings(&db, &params.interval, params.start_time).await {
         Ok(()) => HttpResponse::Ok().body("Earnings fetched and stored successfully."),
         Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
