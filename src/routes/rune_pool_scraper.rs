b11a4cf2f44d8167b431338ff718bcbaf3742aac
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-10-06 12:00:05 +0530

    chore: update rpmuh scraper api

--- src/routes/rune_pool_scraper.rs
+++ src/routes/rune_pool_scraper.rs
@@ -5,11 +5,14 @@ use actix_web::{
     HttpResponse,
 };
 use serde::Deserialize;
+use std::env;
+use dotenv::dotenv;
 
 #[derive(Debug, Deserialize)]
 pub struct FetchRunePoolParams {
-    pub interval: String, 
-    pub from: i64,      
+    pub interval: String,
+    pub from: i64,
+    pub secret: String,
 }
 
 #[post("/rune_pool_scraper")]
@@ -17,6 +20,14 @@ pub async fn fetch_and_store_rune_pool(
     db: Data<Database>,
     params: Json<FetchRunePoolParams>,
 ) -> HttpResponse {
+    dotenv().ok();
+
+    let expected_secret = env::var("SECRET_KEY").unwrap_or_else(|_| "default_secret".to_string());
+
+    if params.secret != expected_secret {
+        return HttpResponse::Unauthorized().body("Wrong secret key.");
+    }
+
     match services::rpmuh_fetcher::fetch_and_store_rune_pool_history(&db, &params.interval, params.from).await {
         Ok(()) => HttpResponse::Ok().body("Rune pool history fetched and stored successfully."),
         Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
